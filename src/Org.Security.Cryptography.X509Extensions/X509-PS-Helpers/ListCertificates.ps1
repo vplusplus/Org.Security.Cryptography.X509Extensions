6bb9a75c4d9dab794d494ad2558b7d75c5b9d38a
Venkatesh Ramakrishnan <vplusplus@users.noreply.github.com>
2020-03-30 22:58:21 -0400

    Cosmetics

--- src/Org.Security.Cryptography.X509Extensions/X509-PS-Helpers/ListCertificates.ps1
+++ src/Org.Security.Cryptography.X509Extensions/X509-PS-Helpers/ListCertificates.ps1
@@ -1,24 +1,22 @@
 ﻿
 
-# List of certs
+# List and print
 $certs = Get-ChildItem -Path cert:\CurrentUser\My
-
-# Print Thumbprint
 $certs
 
-# Loop and print specific property
+# Loop and print
 Foreach ($c IN $certs) 
 {
-	Write-Host $c.SubjectName.Name
-	Write-Host $c.Issuer
-	Write-Host $c.Thumbprint
-    Write-Host "HasPrivateKey?" $c.HasPrivateKey
-    Write-Host "SignatureAlgorithm" $c.SignatureAlgorithm.FriendlyName
+   Write-Host $c.SubjectName.Name
+   Write-Host $c.Issuer
+   Write-Host $c.Thumbprint
+   Write-Host "HasPrivateKey?" $c.HasPrivateKey
+   Write-Host "SignatureAlgorithm" $c.SignatureAlgorithm.FriendlyName
 }
 
-# Find cert by Thumbprint, print details
+# Find and print
 $t = "TheThumbprint"
 $c = Get-ChildItem -Path cert:\CurrentUser\My\$t
 Write-Host $c.ToString($true)
 
- 
+
